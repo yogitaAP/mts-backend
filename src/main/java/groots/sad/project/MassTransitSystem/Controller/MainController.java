96112d70ac7d7accd0949ce1b5562757288999a0
Yogita Sharma <yogita@hansel.io>
2018-11-19 14:29:32 +0530

    Added web app resources and files

---
+++ src/main/java/groots/sad/project/MassTransitSystem/Controller/MainController.java
@@ -0,0 +1,17 @@
+package groots.sad.project.MassTransitSystem.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Yogita on 19/11/18.
+ */
+
+@Controller
+public class MainController {
+    @RequestMapping(value="/",method = RequestMethod.GET)
+    public String homepage(){
+        return "index";
+    }
+}
