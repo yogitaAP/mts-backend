227b717ad3b1cbb6a2bf3b0e409af33e711bd2fe
piyush.chaudhari <piyush.kailash.chaudhari@gmail.com>
2018-11-28 14:01:52 +0530

    refactor file upload api to upload a single file

--- src/main/java/groots/sad/project/MassTransitSystem/controller/FileUploadController.java
+++ src/main/java/groots/sad/project/MassTransitSystem/controller/FileUploadController.java
@@ -1,7 +1,7 @@
 package groots.sad.project.MassTransitSystem.controller;
 
-import groots.sad.project.MassTransitSystem.EventSimulator;
-import org.apache.tomcat.util.http.fileupload.FileUtils;
+import groots.sad.project.MassTransitSystem.service.FileUploadService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
@@ -11,94 +11,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 @RestController
 @RequestMapping("/mts/files")
 public class FileUploadController {
 
-    public static final String DISTIBUTION = "distibution";
-    private static String UPLOADED_FOLDER = System.getProperty("user.dir") + "/mtsfiles/";
-
+    @Autowired
+    FileUploadService fileUploadService;
 
     @PostMapping("/upload")
-    public ResponseEntity<?> uploadFiles(@RequestParam("files") MultipartFile[] uploadFiles){
+    public ResponseEntity<?> uploadFiles(@RequestParam("file") MultipartFile uploadFile, @RequestParam("fileType") String fileType) {
 
-        if (uploadFiles.length > 2) {
-            return new ResponseEntity("Cannot upload more than two files at a time", HttpStatus.BAD_REQUEST);
-        }
-
-        String uploadedFileName = Arrays.stream(uploadFiles)
-                                        .map(MultipartFile::getOriginalFilename)
-                                        .filter(x -> !StringUtils.isEmpty(x))
-                                        .collect(Collectors.joining(" , "));
+        String uploadedFileName = uploadFile.getOriginalFilename();
         if (StringUtils.isEmpty(uploadedFileName)) {
             return new ResponseEntity("please select a file!", HttpStatus.OK);
         }
-
-        try {
-
-            saveUploadedFiles(Arrays.asList(uploadFiles));
-
-        } catch (IOException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-        readDataFromFiles(uploadFiles);
-        cleanTheDirectory();
-        return new ResponseEntity("Successfully uploaded - " + uploadedFileName, HttpStatus.OK);
-    }
-
-    private void cleanTheDirectory() {
-
-        try {
-            FileUtils.cleanDirectory(new File(UPLOADED_FOLDER));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void readDataFromFiles(MultipartFile[] uploadFiles) {
-
-        EventSimulator eventSimulator = EventSimulator.getInstance();
-        List<MultipartFile> scannedList = new ArrayList<>();
-        if (uploadFiles.length == 2) {
-            if (uploadFiles[0].getOriginalFilename().contains(DISTIBUTION)) {
-                scannedList.add(uploadFiles[1]);
-                scannedList.add(uploadFiles[0]);
-            } else {
-                scannedList.add(uploadFiles[0]);
-                scannedList.add(uploadFiles[1]);
-            }
-        }
-        for (MultipartFile file : scannedList) {
-            String fileName = file.getOriginalFilename();
-            System.out.println("Reading data from file" + fileName);
-            if (fileName.contains(DISTIBUTION)) {
-                eventSimulator.readPassengerFrequencyData(fileName);
-            } else {
-                eventSimulator.readData(fileName);
-            }
-        }
-    }
-
-    private void saveUploadedFiles(List<MultipartFile> files) throws IOException {
-
-        for (MultipartFile file : files) {
-            if (file.isEmpty()) {
-                continue; //next file pls
-            }
-            byte[] bytes = file.getBytes();
-                Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
-            Files.write(path, bytes);
-        }
-
+        return fileUploadService.uploadFile(uploadFile, fileType);
     }
 }
---
+++ src/main/java/groots/sad/project/MassTransitSystem/service/FileUploadService.java
@@ -0,0 +1,65 @@
+package groots.sad.project.MassTransitSystem.service;
+
+import groots.sad.project.MassTransitSystem.EventSimulator;
+import org.apache.tomcat.util.http.fileupload.FileUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class FileUploadService {
+
+    public static final String PASSENGER_INFO = "passengerInfo";
+    private static String UPLOADED_FOLDER = System.getProperty("user.dir") + "/mtsfiles/";
+
+    public ResponseEntity<?> uploadFile(MultipartFile uploadFile, String fileType) {
+
+        try {
+            saveUploadedFile(uploadFile);
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        readDataFromFile(uploadFile, fileType);
+        cleanTheDirectory();
+        return new ResponseEntity("Successfully uploaded - " + uploadFile.getOriginalFilename(), HttpStatus.OK);
+    }
+
+    private void saveUploadedFile(MultipartFile file) throws IOException {
+
+        if (file.isEmpty()) {
+            return;
+        }
+        byte[] bytes = file.getBytes();
+        Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+        Files.write(path, bytes);
+    }
+
+    private void cleanTheDirectory() {
+
+        try {
+            FileUtils.cleanDirectory(new File(UPLOADED_FOLDER));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readDataFromFile(MultipartFile file, String fileType) {
+
+        EventSimulator eventSimulator = EventSimulator.getInstance();
+        String fileName = file.getOriginalFilename();
+        System.out.println("Reading data from file" + fileName);
+        if (fileType.equals(PASSENGER_INFO)) {
+            eventSimulator.readPassengerFrequencyData(fileName);
+        } else {
+            eventSimulator.readData(fileName);
+        }
+
+    }
+}
