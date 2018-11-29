0faf147026f5eefb5c50965a19d043e85fb2b698
piyush.chaudhari <piyush.kailash.chaudhari@gmail.com>
2018-11-29 11:32:41 +0530

    write update k constants api for system effeciency

--- src/main/java/groots/sad/project/MassTransitSystem/controller/SystemEfficiencyController.java
+++ src/main/java/groots/sad/project/MassTransitSystem/controller/SystemEfficiencyController.java
@@ -1,10 +1,10 @@
-package groots.sad.project.MassTransitSystem.Controller;
+package groots.sad.project.MassTransitSystem.controller;
 
 import groots.sad.project.MassTransitSystem.service.SystemEfficiencyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/mts/systemefficiency")
@@ -18,5 +18,16 @@ public class SystemEfficiencyController {
         return systemEfficiencyService.computeSystemEfficiency();
     }
 
+
+    @PostMapping("/constants")
+    public void updateKConstants(@RequestBody Map<String, String> kConstants) {
+
+        double kSpeed = Double.parseDouble(kConstants.getOrDefault("kSpeed", "0.5"));
+        double kCapacity = Double.parseDouble(kConstants.getOrDefault("kCapacity", "0.5"));
+        double kWaiting = Double.parseDouble(kConstants.getOrDefault("kWaiting", "0.5"));
+        double kBuses = Double.parseDouble(kConstants.getOrDefault("kBuses", "0.5"));
+        double kCombined = Double.parseDouble(kConstants.getOrDefault("kCombined", "0.5"));
+        systemEfficiencyService.setKConstants(kSpeed, kCapacity, kWaiting, kBuses, kCombined);
+    }
 
 }
