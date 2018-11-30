6036c4cb67b7f9cccc6036ee2199b715ee28900b
piyush.chaudhari <piyush.kailash.chaudhari@gmail.com>
2018-11-30 11:51:07 +0530

    add the function to generate random number for passenger frequency distribution

--- src/main/java/groots/sad/project/MassTransitSystem/manager/UniformDistributionCalculator.java
+++ src/main/java/groots/sad/project/MassTransitSystem/manager/UniformDistributionCalculator.java
@@ -1,10 +1,11 @@
 package groots.sad.project.MassTransitSystem.manager;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class UniformDistributionCalculator {
 
     public static int generateRandomNumber(int lowBound,int highBound){
 
-        // Temp logic we can change it later
-        return 10*(lowBound+highBound)/2;
+        return ThreadLocalRandom.current().nextInt(lowBound,highBound+1);
     }
 }
